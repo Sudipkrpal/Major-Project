037ab67c539d36e679c9c6c5094c0150f63ad37f
Sudip Kr Pal <sudipskp16@gmail.com>
2024-05-27 11:25:57 +0530

    committed

--- models/listing.js
+++ models/listing.js
@@ -2,6 +2,7 @@ const { ref } = require("joi");
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 const Review = require("./review.js");
+const { listingSchema } = require("../schema.js");
 
 const listSchema = new Schema({
     title:{
@@ -25,10 +26,6 @@ const listSchema = new Schema({
     ]
 });
 
-listingSchema.post("findOneAndDelete",async(listing)=>{
-    if(listing){
-        await Review.deleteMany({_id :{$in : listing.review}})
-    }
-})
-const Listing = mongoose.model("Listing",listingSchema);
+
+const Listing = mongoose.model("Listing", listingSchema);
 module.exports = Listing;
